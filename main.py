8f564dc597f5266f063624df30756e1212475dca
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-02-03 20:07:12 -0700

    Pushing new tests

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
 #This is homework 1 for Advanced Software Engineering 4300
 #Emiliano Chavez De La Torre
 
-
 def main():
 
-  # Task 1: Introduction to Replit
-  print(hello_Replit())
+    # Task 1: Introduction to Replit
+    print(hello_Replit())
 
-  # Task 2: Varaibles and Data Types
-  print(int_number())
-  print(float_number())
-  print(string())
-  print(boolean())
+    # Task 2: Varaibles and Data Types
+    int_num = 10
+    float_num = 10.10
+    string = "Hello World"
+    boolean = True
 
+    
 
 # Task 1: Introduction to Replit
 def hello_Replit():
-  return "Hello Replit!"
+    return "Hello Replit!"
 
-
-# Task 2: Varaibles and Data Types
-def int_number():
-  return 10
-
-
-def float_number():
-  return 10.10
-
-
-def string():
-  return "Hello World"
-
-
-def boolean():
-  return True
 
 
 if __name__ == "__main__":
-  main()
+    main()
