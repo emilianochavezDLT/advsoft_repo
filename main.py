04579bd6127d98e19f9604130db51751a9c6bbad
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-02-03 20:14:55 -0700

    test changes

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #This is homework 1 for Advanced Software Engineering 4300
 #Emiliano Chavez De La Torre
 
+# Task 2: Varaibles and Data Types
+int_num = 10
+float_num = 10.10
+string = "Hello World"
+boolean = True
+
 def main():
 
     # Task 1: Introduction to Replit
     print(hello_Replit())
-
-    # Task 2: Varaibles and Data Types
-    int_num = 10
-    float_num = 10.10
-    string = "Hello World"
-    boolean = True
 
     
 
