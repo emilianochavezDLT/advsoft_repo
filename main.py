63d37c58141840de4f04320cefc75ed1f0ca9c7f
Replit user <>
2024-02-04 02:43:40 +0000

    small modification from main.py

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 #This is homework 1 for Advanced Software Engineering 4300
 #Emiliano Chavez De La Torre
 
+
 def main():
 
-    # Task 1: Introduction to Replit
-    print(hello_Replit())
+  # Task 1: Introduction to Replit
+  print(hello_Replit())
 
-    # Task 2: Varaibles and Data Types
-    print(int_number())
-    print(float_number())
-    print(string())
-    print(boolean())
+  # Task 2: Varaibles and Data Types
+  print(int_number())
+  print(float_number())
+  print(string())
+  print(boolean())
+
 
 # Task 1: Introduction to Replit
 def hello_Replit():
-    return "Hello Replit!"
+  return "Hello Replit!"
+
 
 # Task 2: Varaibles and Data Types
 def int_number():
-    return 10
+  return 10
+
 
 def float_number():
-    return 10.10
+  return 10.10
+
 
 def string():
-    return "Hello World"
+  return "Hello World"
+
 
 def boolean():
-    return True
+  return True
 
 
 if __name__ == "__main__":
-    main()
+  main()
