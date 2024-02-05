d489774b7dd6c764ce079fe35e8edd0b3bf2f8e8
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-02-05 00:17:30 -0700

    changed the letter on test 5

--- tests/task5_tests.py
+++ tests/task5_tests.py
@@ -1,15 +1,9 @@
 from main import my_favorite_books, student_database
 
 def test_my_favorite_books():
-    assert my_favorite_books() == ["Beginning Python","Linear Algebra for Dummies","The Hobbit"], "should be ['Beginning Python','Linear Algebra for Dummies','The Hobbit']"
+    assert my_favorite_books() == ["Beginning Python","Linear Algebra for Dummies","The Odyssy"], "should be ['Beginning Python','Linear Algebra for Dummies','The Hobbit']"
 
-#This test should fail because it is not the first 3 books, it is the last 2 books
-def fail_test_my_favorite_books():
-    assert my_favorite_books() == ["The Lord of the Rings", "The Odyssey"], "should fail the test"
 
-#This test should fail because it is not the first 3 books, it is the first two and last book
-def fail_test_my_favorite_books_2():
-    assert my_favorite_books() == ["The Hobbit", "The Lord of the Rings", "The Odyssey"], "should fail the test"
 
 def test_student_database():
     assert student_database() == {1:{"name": "John Doe", "student_id": 1234}, 2:{"name": "Jane Doe", "student_id": 5678}}, "look at the main file for the dictionary"
