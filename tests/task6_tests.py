21d468618332ece9cc1bfdf407e2275513f9c459
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-02-06 00:23:27 -0700

    Changed params for tests

--- tests/task6_tests.py
+++ tests/task6_tests.py
@@ -5,16 +5,15 @@ from main import count_words_in_file, text_file_test
 
 #We are going to dynamically generate function names for our pytest tests based on the file names in the directory
 directory = "../"
-@text_file_test(directory)
-def test_count_words_in_file(file_path): #File path is sent to wrapper function of the decorator
+@text_file_test(directory, 8)
+def test_count_words_in_file(file_path, expected_count): #File path is sent to wrapper function of the decorator
     #We are going to use the count_words_in_file function to test the word count in the file
-    expected_count = 8 #We know that one file has 8 words
-    assert count_words_in_file(file_path) == 8
+    assert count_words_in_file(file_path) == expected_count
 
-@text_file_test(directory)
-def test_count_words_in_file2(file_path):
+@text_file_test(directory, 3)
+def test_count_words_in_file2(file_path, expected_count):
     #We are going to use the count_words_in_file function to test the word count in the file
-    assert count_words_in_file(file_path) == 3
+    assert count_words_in_file(file_path) == expected_count
 
 
 
