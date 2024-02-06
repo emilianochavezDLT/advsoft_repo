a9b0721b62073fc01a50992bf9fd50ea7060ddb8
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-02-06 00:27:42 -0700

    Push

--- tests/task6_tests.py
+++ tests/task6_tests.py
@@ -8,12 +8,12 @@ directory = "../"
 @text_file_test(directory, 8)
 def test_count_words_in_file(file_path, expected_count): #File path is sent to wrapper function of the decorator
     #We are going to use the count_words_in_file function to test the word count in the file
-    assert count_words_in_file(file_path) == expected_count
+    assert count_words_in_file(file_path) == expected_count, f"Expected {expected_count} words in {file_path}"
 
 @text_file_test(directory, 3)
 def test_count_words_in_file2(file_path, expected_count):
     #We are going to use the count_words_in_file function to test the word count in the file
-    assert count_words_in_file(file_path) == expected_count
+    assert count_words_in_file(file_path) == expected_count, f"Expected {expected_count} words in {file_path}"
 
 
 
