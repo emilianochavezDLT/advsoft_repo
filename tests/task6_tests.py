1a4d56faa1c39fc456386e945fcbe38998aaa9f7
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-02-05 23:58:30 -0700

    added a test

--- tests/task6_tests.py
+++ tests/task6_tests.py
@@ -6,10 +6,14 @@ from main import count_words_in_file, text_file_test
 #We are going to dynamically generate function names for our pytest tests based on the file names in the directory
 directory = "../"
 @text_file_test(directory)
-def test_count_words_in_file(file_path):
+def test_count_words_in_file(file_path): #File path is sent to wrapper function of the decorator
     #We are going to use the count_words_in_file function to test the word count in the file
     assert count_words_in_file(file_path) == 8
-    assert count_words_in_file(file_path) == 5
+
+@text_file_test(directory)
+def test_count_words_in_file(file_path):
+    #We are going to use the count_words_in_file function to test the word count in the file
+    assert count_words_in_file(file_path) == 3
 
 
 
