e82be8a6a19980410e218078492e0878209e55b7
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-02-06 01:21:37 -0700

    Push

--- tests/task6_tests.py
+++ tests/task6_tests.py
@@ -9,11 +9,19 @@ directory = "../"
 #Putting all of the text files in the directory into a list
 text_files = [("../task6file.txt", 8), ("../task6file2.txt", 5)]
 
-for file, expected_count in text_files:
+def create_test_function(file_path, expected_output):
     def test_count_words_in_file():
-        assert count_words_in_file(file) == expected_count
+        assert count_words_in_file(file_path) == expected_output
+    return test_count_words_in_file
+
+#Dynamically creating test functions for each file in the directory
+for file, exepected_count in text_files:
     test_name = f"test_{file.split('/')[-1].split('.')[0]}"
-    globals[test_name] = test_count_words_in_file
+    test = create_test_function(file, exepected_count)
+    globals()[test_name] = test
+    
+    
+    
 
 
 
