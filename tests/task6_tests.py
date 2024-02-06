80b03dfec41a46e804af70eda6fdace3ca30d41a
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2024-02-06 01:29:16 -0700

    Task6 complete

--- tests/task6_tests.py
+++ tests/task6_tests.py
@@ -1,10 +1,7 @@
 from main import count_words_in_file
-import os #Tech Doc: https://docs.python.org/3/library/os.html#files-and-directories
+
 
 # Task 6: File Handeling and Metaprogramming
-
-#We are going to dynamically generate function names for our pytest tests based on the file names in the directory
-directory = "../"
 
 #Putting all of the text files in the directory into a list
 text_files = [("task6file.txt", 8), ("task6file2.txt", 5)]
@@ -15,10 +12,11 @@ def create_test_function(file_path, expected_output):
     return test_count_words_in_file
 
 #Dynamically creating test functions for each file in the directory
-for file, exepected_count in text_files:
-    test_name = f"test_{file.split('/')[-1].split('.')[0]}"
-    test = create_test_function(file, exepected_count)
-    globals()[test_name] = test
+for file, exepected_count in text_files: #file and expected_count are tuples and are from the text_files list
+    #So here we are splitting the file path and getting the name of the file without the extension
+    test_name = f"test_{file.split('/')[-1].split('.')[0]}" #This is the name of the test function
+    test = create_test_function(file, exepected_count) #This is the function that we are going to create
+    globals()[test_name] = test #This is creating the function in the global scope
     
     
     
