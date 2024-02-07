ec78ddacd20161b62f766375788995c649658145
Replit user <>
2024-02-07 04:06:33 +0000

    Packages

--- tests/task6_tests.py
+++ tests/task6_tests.py
@@ -1,5 +1,5 @@
 from main import count_words_in_file
-import os #Tech Doc: https://docs.python.org/3/library/os.html#files-and-directories
+import os  #Tech Doc: https://docs.python.org/3/library/os.html#files-and-directories
 
 # Task 6: File Handeling and Metaprogramming
 
@@ -9,24 +9,17 @@ directory = "../"
 #Putting all of the text files in the directory into a list
 text_files = [("task6file.txt", 8), ("task6file2.txt", 5)]
 
+
 def create_test_function(file_path, expected_output):
-    def test_count_words_in_file():
-        assert count_words_in_file(file_path) == expected_output
-    return test_count_words_in_file
+
+  def test_count_words_in_file():
+    assert count_words_in_file(file_path) == expected_output
+
+  return test_count_words_in_file
+
 
 #Dynamically creating test functions for each file in the directory
 for file, exepected_count in text_files:
-    test_name = f"test_{file.split('/')[-1].split('.')[0]}"
-    test = create_test_function(file, exepected_count)
-    globals()[test_name] = test
-    
-    
-    
-
-
-
-
-
-
-
-
+  test_name = f"test_{file.split('/')[-1].split('.')[0]}"
+  test = create_test_function(file, exepected_count)
+  globals()[test_name] = test
